7f799022bece902111030a6e0dcf218b2a49c7e2
html5syt <html5syt@163.com>
2024-12-28 01:10:19 +0800

    data框

--- src/PhiControls.py
+++ src/PhiControls.py
@@ -1,8 +1,19 @@
 import flet as ft
 import flet.canvas as cv
 
+def hum_convert(value):
+    # 单位：B
+    units = ["B","KB", "MB", "GB", "TB", "PB"]
+    size = 1024.0
+    if value <1024.0:
+        return "0.00 KB"
+    for i in range(len(units)):
+        if (value / size) < 1:
+            return "%.2f %s" % (value, units[i])
+        value = value / size
+
 class PhiBack(ft.Stack):
-    def __init__(self, on_click=None):
+    def __init__(self, on_click=None,n=1):
         super().__init__()
         self.controls = [
             ft.Container(
@@ -10,9 +21,9 @@ class PhiBack(ft.Stack):
                     [
                         cv.Path(
                             [
-                                cv.Path.LineTo(150, 0),
-                                cv.Path.LineTo(124.5448267, 95),
-                                cv.Path.LineTo(0, 95),
+                                cv.Path.LineTo(150*n, 0),
+                                cv.Path.LineTo(124.5448267*n, 95*n),
+                                cv.Path.LineTo(0, 95*n),
                             ],
                             paint=ft.Paint(
                                 style=ft.PaintingStyle.FILL,
@@ -21,19 +32,19 @@ class PhiBack(ft.Stack):
                         ),
                         cv.Path(
                             [
-                                cv.Path.MoveTo(150, 0),
-                                cv.Path.LineTo(157.5, 0),
-                                cv.Path.LineTo(132.0448267, 95),
-                                cv.Path.LineTo(124.5448267, 95),
+                                cv.Path.MoveTo(150*n, 0),
+                                cv.Path.LineTo(157.5*n, 0),
+                                cv.Path.LineTo(132.0448267*n, 95*n),
+                                cv.Path.LineTo(124.5448267*n, 95*n),
                             ],
                             paint=ft.Paint(
                                 style=ft.PaintingStyle.FILL,
-                                color=ft.colors.WHITE,
+                                color=ft.Colors.WHITE,
                             ),
                         ),
                     ],
-                    width=157.5,
-                    height=95,
+                    width=157.5*n,
+                    height=95*n,
                     expand=True,
                 ),
                 padding=0,
@@ -42,7 +53,10 @@ class PhiBack(ft.Stack):
             ft.Container(
                 ft.Image(src="back.svg"),
                 # alignment=ft.alignment.center,
-                margin=ft.margin.only(top=26, left=44),
+                margin=ft.margin.only(top=26*n, left=44*n),
                 on_click=on_click,
+                width=40*n,
+                height=40*n,
             ),
         ]
+        
--- src/main.py
+++ src/main.py
@@ -2,19 +2,64 @@ import math
 import flet as ft
 import PhiControls as phi
 import random
+import flet.canvas as cv
+
+DATA = 0.0
+
 
 class PhiData(ft.Stack):
-    def __init__(self, on_click=None):
+    datatemp = 0.0
+
+    def __init__(self, on_click=None, n=0.7):
+        self.datatemp
         super().__init__()
+        n = 1.15 * n
         self.controls = [
             ft.Stack(
                 [
-                    ft.Image(src='Data.png')
-                ]
+                    ft.Image(src="Data.png", width=350 * n),
+                    ft.ResponsiveRow(
+                        [
+                            ft.Image(
+                                src="dataicon.png",
+                                col=0.62,
+                                height=70 * n,
+                            ),
+                            ft.Container(
+                                ft.Text(
+                                    spans=[
+                                        ft.TextSpan(
+                                            "29.71 MB",
+                                            ft.TextStyle(
+                                                size=32 * n, color=ft.Colors.WHITE
+                                            ),
+                                        ),
+                                        ft.TextSpan(
+                                            "  Data",
+                                            ft.TextStyle(
+                                                size=27 * n, color=ft.Colors.WHITE
+                                            ),
+                                        ),
+                                    ],
+                                ),
+                                col=1.32,
+                                alignment=ft.alignment.top_left,
+                                margin=ft.margin.only(top=15 * n),
+                            ),
+                        ],
+                        alignment=ft.alignment.top_left,
+                        columns=2,
+                        spacing=0,
+                    ),
+                ],
+                width=350 * n,
             )
         ]
 
+
 def main(page: ft.Page):
+    global DATA
+
     # layout debug
     def on_keyboard(e: ft.KeyboardEvent):
         # print(e)
@@ -32,6 +77,11 @@ def main(page: ft.Page):
     page.fonts = {
         "Exo": "Exo-Regular.otf",
     }
+    if page.platform == ft.PagePlatform.ANDROID or page.platform == ft.PagePlatform.IOS:
+        # 缩放倍数
+        n = 0.5
+    else:
+        n = 0.7
 
     page.theme = ft.Theme(font_family="Exo")  # 默认应用字体
 
@@ -45,10 +95,11 @@ def main(page: ft.Page):
         ft.Stack(
             [
                 ft.Stack(
+                    # 背景层
                     [
                         ft.Image(
-                            # str(random.randint(1, 119)) + ".png",
-                            src="14.png",
+                            str(random.randint(1, 119)) + ".png",
+                            # src="14.png",
                             fit=ft.ImageFit.COVER,
                             expand=True,
                         ),
@@ -58,11 +109,24 @@ def main(page: ft.Page):
                     fit=ft.StackFit.EXPAND,
                     expand=True,
                 ),
-                ft.Container(
-                    phi.PhiBack(on_click=lambda e: page.window.close()),
-                    margin=ft.margin.only(top=6),
+                ft.ResponsiveRow(
+                    [
+                        ft.Container(
+                            # 返回
+                            phi.PhiBack(on_click=lambda e: page.window.close(), n=n),
+                            margin=ft.margin.only(top=8 * n),
+                            col=1,
+                        ),
+                        ft.Container(
+                            # data
+                            PhiData(n=n),
+                            col=1,
+                            alignment=ft.alignment.top_right,
+                            margin=ft.margin.only(right=25 * n),
+                        ),
+                    ],
+                    columns=2,
                 ),
-                
             ],
             alignment=ft.alignment.center,
             fit=ft.StackFit.EXPAND,
