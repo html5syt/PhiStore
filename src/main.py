382dd64d11b61544173316014a41d521d593271f
html5syt <html5syt@163.com>
2025-01-25 23:01:29 +0800

    抽奖......改了一堆

--- src/main.py
+++ src/main.py
@@ -1,26 +1,14 @@
 import asyncio
-import math
-import time
 import flet as ft
 import PhiControls as phi
 import random
-import flet.canvas as cv
 
 lock = asyncio.Lock()
 DATA = 0.0
 
-from dataclasses import field
-
-
-class Scale:
-    scale: float = field(default=None)
-    scale_x: float = field(default=None)
-    scale_y: float = field(default=None)
-    alignment: ft.Alignment = field(default=None)
-
 
 class PhiLottery(ft.Stack):
-    def __init__(self, on_click=None, n=1, page=ft.Page):
+    def __init__(self, n=1, page=ft.Page):
         if (
             page.platform == ft.PagePlatform.ANDROID
             or page.platform == ft.PagePlatform.IOS
@@ -29,7 +17,7 @@ class PhiLottery(ft.Stack):
         super().__init__()
         self.controls = [
             ft.Image(
-                src="phi0101.webp",
+                src="phi0101.webp", 
                 width=350 * n,
                 offset=ft.transform.Offset(0, 0),
                 # animate_offset=ft.animation.Animation(100),
@@ -44,46 +32,43 @@ class PhiLottery(ft.Stack):
                         width=350 * n,
                         alignment=ft.alignment.center,
                         padding=0,
-                        scale=ft.transform.Scale(scale_x=0.8, scale_y=0),
-                        # border=ft.border.all(1, ft.Colors.WHITE),
-                        # visible=False,
-                        # margin=ft.margin.only(top=200 * n),
-                        # offset=ft.transform.Offset(0, 0),
+                        scale=ft.transform.Scale(scale_x=0.7, scale_y=0),
+                        margin=0,   
                     ),
                     ft.Text(
                         "",
                         color=ft.Colors.WHITE,
                         size=30 * n,
                         text_align=ft.TextAlign.CENTER,
-                        # visible=False,
                         width=350 * n,
+                        style=ft.TextStyle(height=1),
+                        offset=ft.transform.Offset(0, -1.1*n),
                     ),
+
                 ],
                 expand=True,
-                # width=350 * n,
-                alignment=ft.alignment.top_center,
+                alignment=ft.MainAxisAlignment.START,
                 spacing=0,
                 offset=ft.transform.Offset(0, 0),
                 opacity=0,
                 animate_offset=300,
                 animate_opacity=300,
             ),
-            # phi.PhiBack(on_click=lambda e: page.window.close(), n=n),
         ]
 
-    async def on_click(self, e=None, page=None, n=0.8):
+    async def on_click(self, e=None, page=None, n=0.8,multi=False):
         async with (
             lock
         ):  # 确保只有一个 on_click 在执行，点几次执行几次，无忽略（写不动了
-            # 注：连抽功能未实现
+            # 注：连抽功能未实现，方法已给出
             if page and (
                 page.platform == ft.PagePlatform.ANDROID
                 or page.platform == ft.PagePlatform.IOS
             ):
                 n = n * 1.2
 
-            detail = self.controls[1].controls[0]
-            detailText = self.controls[1].controls[1]
+            detail = self.controls[1].controls[0]  # 图标
+            detailText = self.controls[1].controls[1]  # 描述
             detailText.size = 30 * n
             self.controls[0].visible = not self.controls[0].visible  # ?图
 
@@ -91,26 +76,30 @@ class PhiLottery(ft.Stack):
             self.controls[1].animate_opacity = 300
             page.update()
             if not self.controls[0].visible:
-                # detail.visible = True
-                # detailText.visible = True
                 self.controls[1].animate_offset = 1
                 self.controls[1].animate_opacity = 1
                 detail.animate = 300
                 self.controls[1].opacity = 1
                 self.controls[1].offset = ft.transform.Offset(0, 0)
                 page.update()
+                # 初始状态 -> 逐渐显示
                 detailText.value = ""
+                # 对接抽奖函数
                 text = "2 MB"
-                for i in range(1,24):
-                    detail.scale = ft.transform.Scale(scale_x=0.8, scale_y=0.8/23*i)
+                detail.content.src = "avatar.png"
+                # 存在BUG：手机电脑显示效果不一样
+                
+                for i in range(1, 24):
+                    detail.scale = ft.transform.Scale(scale_x=0.7, scale_y=0.7 / 23 * i)
                     page.update()
-                    await asyncio.sleep(0.01)
+                    await asyncio.sleep(0.016)
                     # scale_x和scale_y无法使用动画，只能用这种方法
                 for textTemp in text:
                     detailText.value += textTemp
                     page.update()
                     await asyncio.sleep(0.05)
             else:
+                # 逐渐隐藏 -> 初始状态
                 self.controls[1].animate_offset = 300
                 self.controls[1].animate_opacity = 300
                 page.update()
@@ -119,10 +108,10 @@ class PhiLottery(ft.Stack):
                     -1 * (page.width // 4 / detail.width), 0
                 )
                 page.update()
-                # await asyncio.sleep(0.7)
-                await asyncio.sleep(0.5)
-            print(self.controls[1].animate_offset)
-            print(self.controls[1].animate_opacity)
+                if multi:
+                    await asyncio.sleep(0.1)
+                else:
+                    await asyncio.sleep(0.5)
 
 
 async def main(page: ft.Page):
@@ -163,8 +152,36 @@ async def main(page: ft.Page):
     datashow = phi.PhiData(n=n)
     lottery = PhiLottery(n=n, page=page)
 
+
+
     async def lottery_on_click(e):
+        nonlocal lottery
+        lottery.controls[0].src = "phi0101.webp"
         await PhiLottery.on_click(self=lottery, page=page, n=n)
+    async def lottery_on_click_multi(e):
+        nonlocal lottery
+        lottery.controls[0].src = " "
+        # 连抽
+        for i in range(1, 21):
+            await PhiLottery.on_click(self=lottery, page=page, n=n,multi=True)
+        lottery.controls[0].src = "phi0101.webp"
+        page.update()
+        # nonlocal multi0
+        # multi0 = [0, 1]
+        # # 连抽
+        # if multi0==[0,1]:
+        #     multi0=[1,1]
+        #     await PhiLottery.on_click(self=lottery, page=page, n=n)
+        #     print(multi0)
+        # elif multi0==[1,1] or multi0[1]<10: #连抽中
+        #     for i in range(1, 3):
+        #         await PhiLottery.on_click(self=lottery, page=page, n=n)
+        #     multi0[1]+=1
+        #     print(multi0)
+        # elif multi0[1]>=10: #连抽结束
+        #     multi0=[0,1]
+        # 单抽
+        # await PhiLottery.on_click(self=lottery, page=page, n=n)
 
     # 页面组件树
     page.add(
@@ -227,7 +244,7 @@ async def main(page: ft.Page):
                 ft.Container(
                     lottery,
                     margin=ft.margin.only(top=160 * n),
-                    alignment=ft.alignment.center,
+                    alignment=ft.alignment.top_center,
                     expand=True,
                 ),
                 ft.Container(
@@ -238,6 +255,15 @@ async def main(page: ft.Page):
                     padding=ft.padding.all(10),
                     alignment=ft.alignment.top_center,
                     margin=ft.margin.only(top=550 * n),
+                ),
+                ft.Container(
+                    ft.Button(
+                        "test multi",
+                        on_click=lottery_on_click_multi,
+                    ),
+                    padding=ft.padding.all(10),
+                    alignment=ft.alignment.top_center,
+                    margin=ft.margin.only(top=600 * n),
                 ),
             ],
             alignment=ft.alignment.center,
